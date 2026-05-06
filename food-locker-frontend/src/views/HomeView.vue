868c1d268e96634f8b2fac7281c2f898ff588c69
Beck049 <beckxiao2001323@gmail.com>
2026-05-06 19:15:44 +0800

    [fix] for deploymemt

--- food-locker-frontend/src/views/HomeView.vue
+++ food-locker-frontend/src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 
 import { useAuthenticator } from '@aws-amplify/ui-vue';
 import { computed, onMounted, ref, toRefs, watch } from 'vue';
